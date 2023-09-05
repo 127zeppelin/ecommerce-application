61deb4a68bae7070bbaa8b1d2b0f85358dfc91ba
127zeppelin <127.zeppelin@gmail.com>
2023-09-05 03:23:43 +0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -5,3 +5,4 @@ import { isTheUserLoggedIn } from './pages/login/istheuserlogged'
 const app = new App()
 app.run()
 isTheUserLoggedIn()
+alert('Просим проверить работу в четверг, у нас ушел Тим Лид из команды на втором спринте и до сих пор никак не можем наверстать')
