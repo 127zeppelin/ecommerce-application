b443a0f0fad771a24751c0385b871bd74ba1c06e
127zeppelin <127.zeppelin@gmail.com>
2023-09-19 03:09:43 +0300

    Update addincart.ts

--- src/pages/car/addincart.ts
+++ src/pages/car/addincart.ts
@@ -50,7 +50,6 @@ const createCard = (productId: string, quantityCar: number, acessToken: string) 
 }
 
 export const addInCart = (/*idCart: string, */idProduct: string, quantityCar: number) => {
-  const acessTokenL0calStorage = localStorage.getItem('access_token');
   //const requestHeader = acessTokenL0calStorage !== undefined ? {
   //   Authorization: `Bearer ${acessTokenL0calStorage}`,
   // } : tokenCache;
@@ -60,13 +59,5 @@ export const addInCart = (/*idCart: string, */idProduct: string, quantityCar: nu
   const tokenTime = currentToken.expirationTime
   const theToken = `Bearer ${token}`
   console.log('ТОкен', token, tokenTime, tokenRf)
-  const request = createCard(idProduct, quantityCar, theToken)
-
-    .then(response => {
-      console.log('Ответ апи добавление машины', response);
-    })
-    .catch(error => {
-      console.error('Произошла ошибка при выполнении запроса', error);
-    });
 }
 
