8953b22bd7557065bc55ab94ce88b6a6e38ad002
127zeppelin <127.zeppelin@gmail.com>
2023-09-21 10:38:59 +0300

    feat: add styles for mobile devices

--- src/components/header/header.ts
+++ src/components/header/header.ts
@@ -4,16 +4,17 @@ import {
   logoutAndRedirect,
   isTheUserLoggedIn,
 } from '../../pages/login/istheuserlogged'
+import { createHtmlElement } from '../../utils/createelement'
 
 class Header extends Component {
-  private createPageButtons(href: string, text: string,  html: string|undefined) {
+  private createPageButtons(href: string, text: string, html: string | undefined) {
     const pageButton = document.createElement('div')
     const menuBtn = document.createElement('a')
     pageButton.className = CSS_CLASSES.menuBtn
     menuBtn.setAttribute('href', href)
 
-    if(text !== ''){menuBtn.innerText = text}
-    if(html){menuBtn.innerHTML = `<img src="${html}">`}
+    if (text !== '') { menuBtn.innerText = text }
+    if (html) { menuBtn.innerHTML = `<img src="${html}">` }
     menuBtn.setAttribute('id', `btn-${href.slice(1)}`)
     pageButton.append(menuBtn)
     return pageButton
@@ -29,7 +30,7 @@ class Header extends Component {
     const pageButtons = document.createElement('div')
     pageButtons.className = 'menu_wrapper'
 
-    const mainPageButton: HTMLElement = this.createPageButtons('#main', 'Main',undefined)
+    const mainPageButton: HTMLElement = this.createPageButtons('#main', 'Main', undefined)
     pageButtons.append(mainPageButton)
 
     const carsButton = this.createPageButtons('#cars', 'Cars', undefined)
@@ -58,9 +59,38 @@ class Header extends Component {
       logautButton.addEventListener('click', logoutAndRedirect)
       pageButtons.append(logautButton)
     }
-    const CardButton = this.createPageButtons('#cart', '','./images/cart-full.svg')
+    const CardButton = this.createPageButtons('#cart', '', './images/cart-full.svg')
     pageButtons.append(CardButton)
 
+    const burgerBtn = createHtmlElement({
+      tagName: 'div',
+      cssClass: [CSS_CLASSES.burgerBtn]
+    })
+    title.append(burgerBtn)
+
+    burgerBtn.addEventListener('click', () => {
+      pageButtons.classList.toggle('active');
+      if (document.body.style.overflow === 'hidden') {
+        document.body.style.overflow = 'auto';
+      } else {
+        document.body.style.overflow = 'hidden';
+      }
+    })
+    pageButtons.addEventListener('click', (event) => {
+      const clickedElement = event.target as HTMLElement;
+      if (clickedElement && clickedElement.tagName === 'A') {
+        if (document.body.style.overflow === 'hidden') {
+          document.body.style.overflow = 'auto';
+        }
+      }
+    });
+    const burgerLine = createHtmlElement({
+      tagName: 'span',
+      cssClass: [CSS_CLASSES.burgerLine]
+    })
+    burgerBtn.append(burgerLine)
+
+    title.append(pageButtons)
 
     title.append(pageButtons)
     this.container.append(title)
